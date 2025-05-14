f58d5abc3f432603005447dad0afed2e880ec85a
lolosaisa <lwamaitha3924@gmail.com>
2025-05-14 09:52:53 +0300

    finished setting up the frontend of the project

---
+++ miniminds_frontend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Miniminds a decentralized e-learning platform",
+  description: "education infrastructure designed to empower students, teachers, institutions, and guardians by enabling transparent, personalized, and incentive-driven learning experiences — starting in under-resourced and high-volume learning environments.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ miniminds_frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust if you're not using `src/`
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config;
